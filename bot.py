ac8b5f3434fc4d9cd651a1d8c73537d19e16c9e0
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-11-12 12:49:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,49 +43,49 @@ LazyPrincessBot.start()
 loop = asyncio.get_event_loop()
 
 
-   async def Lazy_start():
-       print('\n')
-       print('Initalizing Lazy Bot')
-       bot_info = await LazyPrincessBot.get_me()
-       LazyPrincessBot.username = bot_info.username
-       await initialize_clients()
-       for name in files:
-           with open(name) as a:
-               patt = Path(a.name)
-               plugin_name = patt.stem.replace(".py", "")
-               plugins_dir = Path(f"plugins/{plugin_name}.py")
-               import_path = "plugins.{}".format(plugin_name)
-               spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
-               load = importlib.util.module_from_spec(spec)
-               spec.loader.exec_module(load)
-               sys.modules["plugins." + plugin_name] = load
-               print("Imported => " + plugin_name)
-        if ON_HEROKU:
-            asyncio.create_task(ping_server())
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats
-        await super().start()
-        await Media.ensure_indexes()
-        me = await LazyPrincessBot.get_me()
-        temp.ME = me.id
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.username = '@' + me.username
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        logging.info(LOG_STR)
-        logging.info(script.LOGO)
-        tz = pytz.timezone('Asia/Kolkata')
-        today = date.today()
-        now = datetime.now(tz)
-        time = now.strftime("%H:%M:%S %p")
-        await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
-        await idle()
-  
+async def Lazy_start():
+    print('\n')
+    print('Initalizing Lazy Bot')
+    bot_info = await LazyPrincessBot.get_me()
+    LazyPrincessBot.username = bot_info.username
+    await initialize_clients()
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem.replace(".py", "")
+            plugins_dir = Path(f"plugins/{plugin_name}.py")
+            import_path = "plugins.{}".format(plugin_name)
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+            load = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(load)
+            sys.modules["plugins." + plugin_name] = load
+            print("Lazy Imported => " + plugin_name)
+    if ON_HEROKU:
+        asyncio.create_task(ping_server())
+    b_users, b_chats = await db.get_banned()
+    temp.BANNED_USERS = b_users
+    temp.BANNED_CHATS = b_chats
+    await Media.ensure_indexes()
+    me = await LazyPrincessBot.get_me()
+    temp.ME = me.id
+    temp.U_NAME = me.username
+    temp.B_NAME = me.first_name
+    LazyPrincessBot.username = '@' + me.username
+    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+    logging.info(LOG_STR)
+    logging.info(script.LOGO)
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    now = datetime.now(tz)
+    time = now.strftime("%H:%M:%S %p")
+    await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app, bind_address, PORT).start()
+    await idle()
+
+
 if __name__ == '__main__':
     try:
         loop.run_until_complete(Lazy_start())
