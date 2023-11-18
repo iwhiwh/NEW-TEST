ed585fc8fcf34112ee65abdf5ec48505faae2cec
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-11-18 07:06:08 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -209,6 +209,7 @@ async def next_page(bot, query):
             )
         elif off_set is None:
             btn.append([InlineKeyboardButton("𝐏𝐀𝐆𝐄", callback_data="pages"), InlineKeyboardButton(f"{math.ceil(int(offset)/10)+1} / {math.ceil(total/10)}", callback_data="pages"), InlineKeyboardButton("𝐍𝐄𝐗𝐓 ➪", callback_data=f"next_{req}_{key}_{n_offset}")]
+                      )
         else:
             btn.append(
                 [
